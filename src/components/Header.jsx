218e1bdcfc13f5b0f480538853e0d51d0e1d872e
gssp4167 <gssp4167@gmail.com>
2023-12-22 11:08:38 +0530

    BUG:Fixed shortlist icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <nav className='flex flex-row items-center justify-between px-4 py-4 border-b-[1px] border-solid border-gray-200' >
  
-        <img src="src/assets/icon.svg" alt="" />
+        <img src="assets/icon.svg" alt="" />
 
         <h3 className='text-[#716966] text-[24px] font-bold' >EmptyCup</h3>
 
